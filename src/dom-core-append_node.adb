cca276461d79e0e59f776dde050cb9855beae285
Simon Wright <simon@pushface.org>
2021-08-28 15:39:58 +0100

    Updated to GLPv3 & RTLE
      * COPYING: removed.
      * COPYING.RUNTIME: new.
      * COPYING3: new.
    
      * src/dom-core-append_node.adb, src/dom-core-append_node.ads,
        src/mckae-xml-xpath-dfs_processing.adb,
        src/mckae-xml-xpath-dfs_processing.ads,
        src/mckae-xml-xpath-expressions.adb,
        src/mckae-xml-xpath-expressions.ads,
        src/mckae-xml-xpath-locations.adb,
        src/mckae-xml-xpath-locations.ads,
        src/mckae-xml-xpath-node_sets.adb,
        src/mckae-xml-xpath-node_sets.ads,
        src/mckae-xml-xpath-predicates-evaluation-evaluators.adb,
        src/mckae-xml-xpath-predicates-evaluation-evaluators.ads,
        src/mckae-xml-xpath-predicates-evaluation.adb,
        src/mckae-xml-xpath-predicates-evaluation.ads,
        src/mckae-xml-xpath-predicates.adb,
        src/mckae-xml-xpath-predicates.ads,
        src/mckae-xml-xpath-query_handling.adb,
        src/mckae-xml-xpath-query_handling.ads, src/mckae-xml-xpath-xia.adb,
        src/mckae-xml-xpath-xia.ads, src/mckae-xml-xpath-xia_worker.adb,
        src/mckae-xml-xpath-xia_worker.ads, src/mckae-xml-xpath.ads,
        src/mckae-xml.ads, src/mckae.ads, test/bench_xpath.adb,
        test/test_xpath.adb: updated copyright notice to GPLv3, RTLE.
    
      * src/mckae-space-unbounded_string_expandable.adb,
        src/mckae-space-unbounded_string_expandable.ads, src/mckae-space.ads:
          removed.

--- src/dom-core-append_node.adb
+++ src/dom-core-append_node.adb
@@ -1,40 +1,34 @@
-------------------------------------------------------------------------
---                                                                    --
---                     McKae Software Utilities                       --
---                                                                    --
---           Copyright (C) 2004 McKae Technologies                    --
---                                                                    --
--- The  McKae   software  utilities   are  free  software;   you  can --
--- redistribute it  and/or modify it  under terms of the  GNU General --
--- Public  License  as published  by  the  Free Software  Foundation; --
--- either version  2, or (at  your option) any later  version.  McKae --
--- Software Utilities are  distributed in the hope that  they will be --
--- useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the  implied --
--- warranty of  MERCHANTABILITY or FITNESS FOR  A PARTICULAR PURPOSE. --
--- See the GNU  General Public License for more  details.  You should --
--- have received a copy of the GNU General Public License distributed --
--- with DTraq; see file COPYING.   If not, write to the Free Software --
--- Foundation, 59  Temple Place -  Suite 330, Boston,  MA 02111-1307, --
--- USA.                                                               --
---                                                                    --
--- As a  special exception, if other files  instantiate generics from --
--- this unit,  or you link this  unit with other files  to produce an --
--- executable,  this unit  does  not by  itself  cause the  resulting --
--- executable to be covered by  the GNU General Public License.  This --
--- exception does  not however invalidate  any other reasons  why the --
--- executable file might be covered by the GNU Public License.        --
---                                                                    --
--- The McKae Software Utilities  are maintained by McKae Technologies --
--- (http://www.mckae.com).                                            --
-------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+--                                                                           --
+--            XPATH IN ADA Copyright (C) 2003, McKae Technologies            --
+--                                                                           --
+--  XPath in  Ada (XIA) is  free software;  you can redistribute  it and/or  --
+--  modify it under terms of the GNU General Public License as published by  --
+--  the Free Software Foundation; either version 3, or (at your option) any  --
+--  later version.  GNAT is distributed in the hope that it will be useful,  --
+--  but  WITHOUT  ANY  WARRANTY;  without  even  the  implied  warranty  of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--                                                                           --
+--  As  a special  exception under  Section  7 of  GPL version  3, you  are  --
+--  granted  additional permissions  described in  the GCC  Runtime Library  --
+--  Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                           --
+--  You should have received a copy of the GNU General Public License and a  --
+--  copy of the GCC Runtime Library  Exception along with this program; see  --
+--  the  files  COPYING3 and  COPYING.RUNTIME  respectively.   If not,  see  --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+--  XIA is maintained at https://github.com/simonjwright/xia                 --
+--                                                                           --
+-------------------------------------------------------------------------------
 
--- Append a node to an XMLAda DOM node list
+--  Append a node to an XMLAda DOM node list
 procedure Dom.Core.Append_Node
   (List : in out Node_List;
-   -- list to which to append the node
+   --  list to which to append the node
 
    N : Node
-   -- Node to append
+   --  Node to append
   ) is
    Old : Node_Array_Access := List.Items;
 begin
--- src/mckae-xml-xpath-xia_worker.ads
+++ src/mckae-xml-xpath-xia_worker.ads
@@ -1,41 +1,27 @@
-----------------------------------------------------------------------
---                                                                  --
---     XIA - XPath In Ada - An XPath Implementation for Ada         --
---                                                                  --
---           Copyright (C) 2014 McKae Technologies                  --
---                                                                  --
--- This library  is free software;  you can redistribute  it and/or --
---  modify  it under  terms of  the GNU  General Public  License as --
---  published by the Free Software Foundation; either version 2, or --
---  (at your option) any later  ver- sion.  DTraq is distributed in --
---  the  hope that  it will  be useful,  but WITHOUT  ANY WARRANTY; --
---  without even the implied warranty of MERCHANTABILITY or FITNESS --
---  FOR A  PARTICULAR PURPOSE.  See the GNU  General Public License --
---  for more details.   You should have received a  copy of the GNU --
---  General  Public  License   distributed  with  DTraq;  see  file --
---  COPYING.   If not, write  to the  Free Software  Foundation, 59 --
---  Temple Place  - Suite  330, Boston, MA  02111-1307, USA.   As a --
---  special  exception, if  other files  instantiate  generics from --
---  this unit, or you link this unit with other files to produce an --
---  executable, this  unit does not  by itself cause  the resulting --
---  executable  to be covered  by the  GNU General  Public License. --
---  This exception  does not  however invalidate any  other reasons --
---  why  the executable  file might  be covered  by the  GNU Public --
---  License.    DTraq   is   maintained   by   McKae   Technologies --
---  (http://www.dtraq.com).                                         --
-----------------------------------------------------------------------
--- XIA (XPathInAda) is a native Ada implementation for querying XML
---  documents using the XPath 1.0 query language.
-----------------------------------------------------------------------
---
--- Known limitations:
---
--- The core library function "id" is not yet implemented, as the
---  XMLAda function Get_Element_By_ID is not yet implemented.
---  Therfore invoking "id" will always result in an empty node-set.
---
-----------------------------------------------------------------------
-
+-------------------------------------------------------------------------------
+--                                                                           --
+--            XPATH IN ADA Copyright (C) 2003, McKae Technologies            --
+--     XPATH IN ADA Copyright (C) 2021, Simon Wright <simon@pushface.org>    --
+--                                                                           --
+--  XPath in  Ada (XIA) is  free software;  you can redistribute  it and/or  --
+--  modify it under terms of the GNU General Public License as published by  --
+--  the Free Software Foundation; either version 3, or (at your option) any  --
+--  later version.  GNAT is distributed in the hope that it will be useful,  --
+--  but  WITHOUT  ANY  WARRANTY;  without  even  the  implied  warranty  of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--                                                                           --
+--  As  a special  exception under  Section  7 of  GPL version  3, you  are  --
+--  granted  additional permissions  described in  the GCC  Runtime Library  --
+--  Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                           --
+--  You should have received a copy of the GNU General Public License and a  --
+--  copy of the GCC Runtime Library  Exception along with this program; see  --
+--  the  files  COPYING3 and  COPYING.RUNTIME  respectively.   If not,  see  --
+--  <http://www.gnu.org/licenses/>.                                          --
+--                                                                           --
+--  XIA is maintained at https://github.com/simonjwright/xia                 --
+--                                                                           --
+-------------------------------------------------------------------------------
 
 with Dom.Core;
 
